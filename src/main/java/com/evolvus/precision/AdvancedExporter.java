30d3d917ba8367c1e4ce3efd799d9e0f015175d0
Prashant <prashant.maroli@evolvussolutions.com>
2021-09-08 16:29:27 +0400

    Update

--- src/main/java/com/evolvus/precision/AdvancedExporter.java
+++ src/main/java/com/evolvus/precision/AdvancedExporter.java
@@ -33,13 +33,18 @@ import java.util.Date;
 import java.util.Properties;
 import java.util.Scanner;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 
 /**
 Precision 100 Advanced Exporter
  */
 public class AdvancedExporter {
+
+
+   private static Logger LOGGER = LoggerFactory.getLogger(AdvancedExporter.class);
 
 
     //Output
@@ -90,8 +95,7 @@ public class AdvancedExporter {
           hkConfig.setJdbcUrl(prop.getProperty("db.jdbcUrl"));
           hkConfig.setUsername(prop.getProperty("db.user"));
           hkConfig.setPassword(prop.getProperty("db.password"));
-          Integer mx = new Integer(prop.getProperty("db.maxPoolSize"));
-          hkConfig.setMaximumPoolSize(mx);
+          hkConfig.setMaximumPoolSize(Integer.parseInt(prop.getProperty("db.maxPoolSize")));
           hkConfig.addDataSourceProperty("cachePrepStmts", prop.getProperty("db.cachePrepStmts"));
           hkConfig.addDataSourceProperty("prepStmtCacheSize", prop.getProperty("db.prepStmtCacheSize"));
           hkConfig.addDataSourceProperty("prepStmtCacheSqlLimit", prop.getProperty("db.prepStmtCacheSqlLimit"));
@@ -101,76 +105,54 @@ public class AdvancedExporter {
 
 
 
-      } catch (IOException ex) {
-          ex.printStackTrace();
+      } catch (IOException e) {
+          LOGGER.error( "Exception when loading properties file {} {}",config,e.getMessage(),e);
       }
     }
 
     public void export(String table) {
+      String csvFileName = getFileName(table);
+      String sql = "SELECT * FROM ".concat(table);
 
+      try(
+        Connection con =
+          ds.getConnection();
+        PreparedStatement statement =
+              con.prepareStatement(sql);
+        ResultSet result =
+              statement.executeQuery();
+        BufferedWriter fileWriter =
+            new BufferedWriter(new FileWriter(csvFileName))
+      ){
+          int columnCount = writeHeaderLine(result,fileWriter);
+          while (result.next()) {
+            String line = "";
+            for (int i = 1; i <= columnCount; i++) {
+              Object valueObject = result.getObject(i);
+              String valueString = "";
 
-        String csvFileName = getFileName(table);
+              if (valueObject != null) valueString = valueObject.toString();
 
+              if (valueObject instanceof String) {
+                  valueString = escapeDoubleQuotes(valueString);
+              }
 
-        Connection con = null;
-        PreparedStatement statement = null;
-        ResultSet result = null;
+              line = line.concat(valueString);
 
-        BufferedWriter fileWriter;
-
-
-        try{
-
-            con = ds.getConnection();
-            String sql = "SELECT * FROM ".concat(table);
-            statement = con.prepareStatement(sql);
-            result = statement.executeQuery();
-
-            fileWriter = new BufferedWriter(new FileWriter(csvFileName));
-            int columnCount = writeHeaderLine(result,fileWriter);
-
-            while (result.next()) {
-                String line = "";
-
-                for (int i = 1; i <= columnCount; i++) {
-                    Object valueObject = result.getObject(i);
-                    String valueString = "";
-
-                    if (valueObject != null) valueString = valueObject.toString();
-
-                    if (valueObject instanceof String) {
-                        valueString = escapeDoubleQuotes(valueString);
-                    }
-
-                    line = line.concat(valueString);
-
-                    if (i != columnCount) {
-                        line = line.concat(delimiter);
-                    }
-                }
-
-                fileWriter.write(line);
-                fileWriter.newLine();
-
+              if (i != columnCount) {
+                  line = line.concat(delimiter);
+              }
             }
-
-            fileWriter.close();
-            result.close();
-            statement.close();
-            con.close();
-
-
+            fileWriter.write(line);
+            fileWriter.newLine();
+          }
         } catch (SQLException e) {
-          Logger lgr = Logger.getLogger(AdvancedExporter.class.getName());
-          lgr.log(Level.WARNING, e.getMessage(), e);
+          LOGGER.error( "SQL Exception when exporting table {} {}",table,e.getMessage());
         } catch (IOException e) {
-          Logger lgr = Logger.getLogger(AdvancedExporter.class.getName());
-          lgr.log(Level.WARNING, e.getMessage(), e);
-        } finally{
-
+          LOGGER.error( "File Exception when exporting table {} {}",table,e.getMessage());
         }
 
-    }
+     }
 
     private String getFileName(String baseName) {
 
@@ -184,11 +166,13 @@ public class AdvancedExporter {
 
     public void processContainerLocation(String loc){
 
+      //LOGGER.info("This is an info level log message!");
+
       File file = new File(loc);
       File[] files = file.listFiles();
-      System.out.println("Processing  Container Location "+file.getAbsolutePath());
+      LOGGER.info("Processing  Container Location {}",file.getAbsolutePath());
       for(File f: files){
-        System.out.println("Processing Container File "+f.getName());
+        LOGGER.info("Processing Container File {}",f.getName());
         new Thread(() -> {
           processContainer(f.getAbsolutePath());
         }).start();
@@ -206,20 +190,19 @@ public class AdvancedExporter {
           String data = myReader.nextLine();
 
           Instant start = Instant.now();
-          System.out.format("Started extracting for table %s at %s%n",data,formatter.format( start ));
+          LOGGER.info("Started extracting for table {} at {}",data,formatter.format( start ));
 
           export(data);
 
           Instant end = Instant.now();
-          System.out.format("Finished extracting for table %s at %s%n",data,formatter.format( end ));
-          System.out.format("Time taken to extract table %s --> %d seconds %n",data,Duration.between(start, end).toMillis()/1000);
+          LOGGER.info("Finished extracting for table {} at {}",data,formatter.format( end ));
+          LOGGER.info("Time taken to extract table {} --> {} seconds",data,Duration.between(start, end).toMillis()/1000);
 
         }
         myReader.close();
 
       } catch (FileNotFoundException e) {
-        Logger lgr = Logger.getLogger(AdvancedExporter.class.getName());
-        lgr.log(Level.WARNING, e.getMessage(), e);
+        LOGGER.error( "File Exception when processing container {} {}",container,e.getMessage(),e);
       }
 
     }
@@ -233,7 +216,6 @@ public class AdvancedExporter {
         // exclude the first column which is the ID field
 
         if(inclHeader){
-          System.out.println("inclHeader");
           for (int i = 1; i <= numberOfColumns; i++) {
               String columnName = metaData.getColumnName(i);
               headerLine = headerLine.concat(columnName).concat(",");
